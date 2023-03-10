3c61413ae4ccf17193e55d869f488883f9411700
Huzit <hwan4789@gmail.com>
2023-03-10 16:49:51 +0900

    2023-03-10 데이터 바인딩 에제

--- app/src/main/java/com/bind/databindingtest/MainActivity.kt
+++ app/src/main/java/com/bind/databindingtest/MainActivity.kt
@@ -2,11 +2,33 @@ package com.bind.databindingtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.bind.databindingtest.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        databinding()
 
+        val a = object: Any(){
+            fun a() = "test"
+        }
+
+        a.a()
+    }
+
+    fun databinding(){
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.main = this
+    }
+    fun ocListener(){
+        Toast.makeText(this, "button is Pressed", Toast.LENGTH_SHORT).show()
     }
 }
